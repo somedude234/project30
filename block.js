50e6ab402a746ed3aa3f20e687e1676d1c3a161e
somedude234 <82318113+somedude234@users.noreply.github.com>
2021-06-19 17:20:58 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,40 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+                     
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("block.png");
+        World.add(world, this.body);
+        this.visibility = 255;
+      }
+     
+
+        display(){
+          if (this.body.speed< 3){
+          var angle = this.body.angle;
+          var pos= this.body.position;
+          push();
+          translate(pos.x, pos.y);
+          rotate(angle);
+          imageMode(CENTER);
+          image(this.image,0,0,this.width, this.height);
+          pop();
+          }
+          else{
+
+            World.remove(world, this.body);
+            push();
+            this.visibility = this.visibility-5;
+            tint(255,this.visibility);
+            image(this.image,this.body.position.x,this.body.position.y,50,50);
+            pop();
+
+          }
+        
+
+      }
+      
+}
